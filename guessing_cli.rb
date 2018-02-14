ee5a70060ef1e1863ebd80c2d60294fb68813627
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-14 13:10:40 -0500

    Bonus - Guessing CLI

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,24 @@
 # Code your solution here!
+def run_guessing_game
+    input = ""
+    while input
+        if input == "exit"
+            exit
+            break
+            else
+            pc = rand(1..6)
+            puts "Guess a number between 1 and 6."
+            input = gets.chomp
+            if input.to_i == pc
+                puts "You guessed the correct number!"
+                else
+                puts "The computer guessed #{pc}."
+            end
+        end
+    end
+end
+
+def exit
+    puts "Goodbye!"
+end
+
